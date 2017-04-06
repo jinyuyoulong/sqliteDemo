b165752f983d2495fd3178b5080055f9b5143613
fanyubing <fanjinlong23@qq.com>
2017-04-06 14:41:38 +0800

    Initial Commit

---
+++ sqliteDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sqliteDemo
+//
+//  Created by fans on 17/4/6.
+//  Copyright © 2017年 Fans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sqliteDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sqliteDemo
+//
+//  Created by fans on 17/4/6.
+//  Copyright © 2017年 Fans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
